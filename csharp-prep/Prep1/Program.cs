0157107123536db14cfae0d6993ebeabd95bfb68
scotty369 <167934023+scotty369@users.noreply.github.com>
2024-04-27 19:40:43 -0600

    Add files via upload

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
-class Program
+class Program1
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Prompt for first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        // Prompt for last name
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        // Display formatted name
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }

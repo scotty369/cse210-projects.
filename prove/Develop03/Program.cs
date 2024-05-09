22e3c09caa6b183f4d58cdc44597d9986b42eb60
scotty369 <167934023+scotty369@users.noreply.github.com>
2024-05-08 21:33:19 -0600

    Update Program.cs

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,5 +1,4 @@
 using System;
-
 public class Word
 {
     public string Text { get; }
@@ -105,7 +104,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        var scripture = new Scripture("John 3:16", "For God so loved the world...");
+        var scripture = new Scripture("Helaman 5:12", "And now, my sons, remember, remember that it is upon the rock of our Redeemer, who is Christ, the Son of God, that ye must build your foundation; that when the devil shall send forth his mighty winds, yea, his shafts in the whirlwind, yea, when all his hail and his mighty storm shall beat upon you, it shall have no power over you to drag you down to the gulf of misery and endless wo, because of the rock upon which ye are built, which is a sure foundation, a foundation whereon if men build they cannot fall.");
 
         do
         {
